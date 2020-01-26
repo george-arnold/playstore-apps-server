9975069950b3845ece19786ed6fc9a36a68a10f4
George Arnold <george.arnold.88@gmail.com>
2020-01-26 17:38:57 -0500

    set up basic test functionality test verifies that endpoint works

--- test/test.app.js
+++ test/test.app.js
@@ -1,3 +1,11 @@
 const { expect } = require('chai');
 const supertest = require('supertest');
 const app = require('../app');
+
+describe('Playstore App', () => {
+  it('should GET /apps', () => {
+    return supertest(app)
+      .get('/apps')
+      .expect(200);
+  });
+});
